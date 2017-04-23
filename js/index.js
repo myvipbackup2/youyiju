8ce42c0c49c5f1041c865204a6b70730cef749bd
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-04-24 01:46:05 +0800

    添加视频弹层

--- js/index.js
+++ js/index.js
@@ -7,19 +7,30 @@ $(function () {
 
     //播放和暂停视频
     /*var video = document.getElementById('recommend-video');
-    var $videoBg = $('#video-bg');
-    var $iconPlay = $('#icon-play');
-    $iconPlay.on('click', function () {
-        console.log(video.paused);
-        if(video.paused){
-            $videoBg.hide();
-            $(this).hide();
-            video.play();
-        }
+     var $videoBg = $('#video-bg');
+     var $iconPlay = $('#icon-play');
+     $iconPlay.on('click', function () {
+     console.log(video.paused);
+     if(video.paused){
+     $videoBg.hide();
+     $(this).hide();
+     video.play();
+     }
+     });
+     video.addEventListener('click', function () {
+     $videoBg.show();
+     $iconPlay.show();
+     video.pause();
+     });*/
+
+    // 视频弹层
+    $('#video').on('click', function () {
+        $('#videoPlayWrap').fadeIn(300);
+        document.getElementById('movie').play();
+
     });
-    video.addEventListener('click', function () {
-        $videoBg.show();
-        $iconPlay.show();
-        video.pause();
-    });*/
+    $('.video_playwrap .close').on('click', function () {
+        $('#videoPlayWrap').fadeOut(300);
+        document.getElementById('movie').pause();
+    })
 });
