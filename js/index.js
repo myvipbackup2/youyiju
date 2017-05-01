216c06bda47aab1990f6340fc9e2fb74db66ae2b
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-05-01 21:58:43 +0800

    修改细节

--- js/index.js
+++ js/index.js
@@ -1,28 +1,4 @@
 $(function () {
-    //精选房源轮播，用的是swiper插件
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
-
-    //播放和暂停视频
-    /*var video = document.getElementById('recommend-video');
-     var $videoBg = $('#video-bg');
-     var $iconPlay = $('#icon-play');
-     $iconPlay.on('click', function () {
-     console.log(video.paused);
-     if(video.paused){
-     $videoBg.hide();
-     $(this).hide();
-     video.play();
-     }
-     });
-     video.addEventListener('click', function () {
-     $videoBg.show();
-     $iconPlay.show();
-     video.pause();
-     });*/
-
     // 视频弹层
     $('#video').on('click', function () {
         $('#videoPlayWrap').fadeIn(300);
